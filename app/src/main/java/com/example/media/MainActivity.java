ddee740f70c2e9ef9075182c76fe024a59a18eb4
rbeninca <rbeninca@gmail.com>
2022-06-07 17:54:13 -0300

    Player

---
+++ app/src/main/java/com/example/media/MainActivity.java
@@ -0,0 +1,162 @@
+package com.example.media;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.MediaTimestamp;
+import android.media.TimedMetaData;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.sql.Timestamp;
+import java.time.format.DateTimeFormatter;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+    private MediaPlayer mediaPlayer;
+    private SeekBar seekBarVolume,seekBarTime;
+    private TextView tvTime,tvNome;
+    private AudioManager audioManager;
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.nerd);
+        seekBarVolume =findViewById(R.id.seekBarVolume);
+        seekBarTime = findViewById(R.id.seekBarTime);
+        tvTime=findViewById(R.id.textViewTime);
+        tvNome=findViewById(R.id.textViewNome);
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        inicializaSeekBar();
+        seekBarVolume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,progress,AudioManager.FLAG_SHOW_UI);
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+        seekBarTime.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                seekBar.setMax(mediaPlayer.getDuration());
+                mediaPlayer.seekTo(seekBarTime.getProgress());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+    }
+
+
+
+
+
+
+
+    public void executaSom(View view){
+        if (this.mediaPlayer!=null){
+            mediaPlayer.start();
+            timerCounter();
+
+
+        }
+    }
+    public  void pauseSom(View view){
+        if (this.mediaPlayer!=null  && mediaPlayer.isPlaying()){
+            mediaPlayer.pause();
+        }
+    }
+    public  void pauseStop(View view){
+        if (this.mediaPlayer!=null && mediaPlayer.isPlaying()){
+            mediaPlayer.stop();
+            mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.nerd);
+        }
+    }
+    public void inicializaSeekBar(){
+        //Configurar o volume
+        int volumeMaximo = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int volumeAtual = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        //COnfigurando volume máximo seekbar
+        seekBarVolume.setMax(volumeMaximo);
+        seekBarVolume.setProgress(volumeAtual);
+    }
+
+    public  void atualizaTime(){
+        String strTimeDuration=convertDurationMillis(mediaPlayer.getDuration());
+        strTimeDuration+="/"+convertDurationMillis(mediaPlayer.getCurrentPosition());
+        tvTime.setText(strTimeDuration);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer!= null && mediaPlayer.isPlaying()){
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer=null;
+
+        }
+    }
+
+    public String convertDurationMillis(Integer getDurationInMillis){
+
+        int getDurationMillis = getDurationInMillis;
+        String convertHours = String.format("%02d", TimeUnit.MILLISECONDS.toHours(getDurationMillis));
+        String convertMinutes = String.format("%02d", TimeUnit.MILLISECONDS.toMinutes(getDurationMillis));
+        String convertSeconds = String.format("%02d", TimeUnit.MILLISECONDS.toSeconds(getDurationMillis) -
+                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(getDurationMillis)));
+
+        String getDuration = convertHours + ":" + convertMinutes + ":" + convertSeconds;
+
+        return getDuration;
+    }
+
+    private Timer timer;
+    private void timerCounter(){
+        timer = new Timer();
+
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        atualizaTime();
+                    }
+                });
+            }
+        };
+        timer.schedule(task, 0, 1000);
+    }
+
+
+
+}
+
